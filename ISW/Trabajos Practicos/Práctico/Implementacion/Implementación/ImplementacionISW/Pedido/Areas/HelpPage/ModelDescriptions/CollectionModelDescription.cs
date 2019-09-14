2e9b1bb8747a2f76cecaaba9ad9ffcd880e6d83c
agustinperalta <aguspm.am@gmail.com>
2019-09-14 14:47:26 -0300

    Modificacion Implementacion

---
+++ ISW/Trabajos Practicos/Práctico/Implementacion/Implementación/ImplementacionISW/Pedido/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Pedido.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
