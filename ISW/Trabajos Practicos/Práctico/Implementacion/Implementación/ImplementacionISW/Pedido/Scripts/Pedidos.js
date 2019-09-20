d942bb8c4425d723bfece4167a9d78006b4cd6a8
agustinperalta <aguspm.am@gmail.com>
2019-09-20 20:38:41 -0300

    Implementacion con mapa
    Falta ver checkbox cuando se selecciona caragar a mano y mapa y validar

---
+++ ISW/Trabajos Practicos/Práctico/Implementacion/Implementación/ImplementacionISW/Pedido/Scripts/Pedidos.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module("PedidoApp", []);
+app.controller("PedidoController", function ($scope) {
+    $scope.modoMapa = false;
+
+    $scope.chk = function () {
+
+        if ($scope.modoMap) {
+            $scope.modoMapa = false;
+        }
+        if ($scope.modoManual) {
+            $scope.modoMapa = true;
+        }
+    };
+
+});
